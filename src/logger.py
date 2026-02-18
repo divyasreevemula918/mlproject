c0ee0ca362dd4dd2d7233274a1a7b80a5f4051d0
divyasree <divyasree5432@gmail.com>
2026-02-18 21:33:47 +0530

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,14 +1,14 @@
 import logging
 import os
 from datetime import datetime
-LOG_FILE=f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.Log"
-LOG_PATH=os.path.join(os.getcwd(),"Logs",LOG_FILE)
-os.makedirs(LOG_PATH,exist_ok=True)
-LOG_FILE_PATH=os.path.join(LOG_PATH,LOG_FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s -%(Levelname)s -%(message)s",
-    Level=logging.INFO
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
 )
 if __name__=="__main__":
