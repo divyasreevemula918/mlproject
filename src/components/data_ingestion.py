ea8edf77d9dfe5dd4419729e90240340c471cc74
divyasree <divyasree5432@gmail.com>
2026-02-19 23:18:47 +0530

    Data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,47 +1,46 @@
-print("FILE STARTED")
-
 import sys
 import os
+from src.exception import CustomException
+from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
-print("IMPORTS DONE")
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts','train.csv')
-    test_data_path: str = os.path.join('artifacts','test.csv')
-    raw_data_path: str = os.path.join('artifacts','data.csv')
-
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','data.csv')
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
-        print("CLASS INITIALIZED ")
-
+        self.ingestion_config=DataIngestionConfig()
     def initiate_data_ingestion(self):
-        print("FUNCTION CALLED")
-
-        df = pd.read_csv('notebook/data/stud.csv')
-        print("CSV READ")
-
-        os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-        print("ARTIFACTS FOLDER READY")
-
-        df.to_csv(self.ingestion_config.raw_data_path, index=False)
-        print("RAW DATA SAVED ")
-
-        train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-        print("DATA SPLIT")
-
-        train_set.to_csv(self.ingestion_config.train_data_path, index=False)
-        test_set.to_csv(self.ingestion_config.test_data_path, index=False)
-        print("TRAIN & TEST SAVED")
-
-        print("INGESTION COMPLETED")
-
-
-if __name__ == "__main__":
-    print("MAIN BLOCK ENTERED")
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+        print("data ingestion method is started")
+        logging.info("entered the data ingestion method")
+        try:
+            print("reading the csv file")
+            df=pd.read_csv('notebook/data/stud.csv')
+            print("csv loaded")
+            logging.info("read the dataset as dataframe")
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            print("ARTIFACTS FOLDER CREATED")
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info("train test spilt initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("ingestio of the data is completed")
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            print("error occured")
+            raise CustomException(e,sys)
+if __name__=="__main__":
+    print("MAIN EXECUTION STARTED")
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_path=train_data,test_path=test_data)
